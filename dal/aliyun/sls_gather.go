e26ec6728277686e93cac57c760de4a1da414a1d
wenhao <hao.wen@deeplang.ai>
2024-08-11 21:07:39 +0800

    fix全链路日志时间排序bug

--- dal/aliyun/sls_gather.go
+++ dal/aliyun/sls_gather.go
@@ -573,7 +573,7 @@ func EndToEndLogsQuery(ctx context.Context, traceId, date string) ([]EntToEndLog
 		return nil, errors.New("query end to end trace logs, error")
 	}
 	sort.Slice(results, func(i, j int) bool {
-		return results[i].Time > results[j].Time
+		return results[i].Time[11:19] > results[j].Time[11:19]
 	})
 	return results, nil
 }

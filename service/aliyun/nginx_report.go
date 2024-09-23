0e3a826a5367563f759d74a10e2174fddbb959f4
wenhao <hao.wen@deeplang.ai>
2024-09-23 10:47:46 +0800

    请求量趋势图差分

--- service/aliyun/nginx_report.go
+++ service/aliyun/nginx_report.go
@@ -451,6 +451,10 @@ func RequestTrendV2(ctx context.Context, req empyrean_lens.RequestTrendReq) (*em
 			sort.Slice(trendItems, func(i, j int) bool {
 				return trendItems[i].Time < trendItems[j].Time
 			})
+			// 对请求量进行差分，计算每个时间戳的请求数量
+			for i := len(trendItems) - 1; i > 0; i-- {
+				trendItems[i].ReqCount = trendItems[i].ReqCount - trendItems[i-1].ReqCount
+			}
 
 			if daysLookback == 0 {
 				data.Data0 = trendItems

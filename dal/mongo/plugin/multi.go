8f3ca9a999ed3636f3eff704b9edb15b996bce0b
zhangtingming <tingming.zhang@deeplang.ai>
2024-10-22 13:55:56 +0800

    链路追踪

--- dal/mongo/plugin/multi.go
+++ dal/mongo/plugin/multi.go
@@ -52,7 +52,10 @@ func (d *MultiDao) FindMultiById(ctx context.Context, id string) (*MultiModel, e
 	var res []*MultiModel
 
 	_id, _ := primitive.ObjectIDFromHex(id)
-	filter := bson.M{"$and": []bson.M{{"is_deleted": false}, {"_id": _id}}}
+	filter := bson.M{"$and": []bson.M{
+		// {"is_deleted": false},
+		{"_id": _id}},
+	}
 	cur, err := pluginCollection.Collection(TableNameMulti).Find(ctx, filter)
 	if err != nil {
 		hlog.CtxErrorf(ctx, "[FindMultiById] mongo find error:%+v", err)

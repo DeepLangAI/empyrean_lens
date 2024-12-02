c5ab48cd7d3bc559d5cd914cf7d55a3f75801ea3
wenhao <hao.wen@deeplang.ai>
2024-12-02 13:48:12 +0800

    多文档大纲树显示

--- static/js/tree.js
+++ static/js/tree.js
@@ -50,6 +50,36 @@ class Tree {
         }
         return tree
     }
+    static parseMultiDoc_OutlineOutput(log){
+        let tree = new Tree('root');
+        let stack = []
+        stack.push(tree.root)
+        for (let i = 0; i < log.length; i++) {
+            let subLog = log[i];
+            if (subLog == "data too long"){
+                continue
+            }
+            // if type is string
+            if (typeof subLog === 'string') {
+                subLog = JSON.parse(subLog)
+            }
+            if (subLog['node_index'] === undefined || subLog['node_index'] === '0'){
+                continue
+            }
+            let level = subLog['node_index'].split('_').length
+            console.log('parsing', i, stack.length, level)
+            while (stack.length > level - 1) {
+                stack.pop()
+            }
+            let parent = stack[stack.length - 1]
+            let value = `【${subLog['headline']}】${subLog['text']}`
+            let child = new TreeNode(value)
+            parent.addChild(child)
+            stack.push(child)
+            console.log('parsing', value)
+        }
+        return tree
+    }
     toDOM(node = this.root) {
         function helper(node, level=0){
             let li = $('<li>').addClass('node');

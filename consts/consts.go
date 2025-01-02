93e79e5e8a26ac89d5e89cd011e0c8d2e86f230f
zhuowenyu <wenyu.zhuo@deeplang.ai>
2025-01-02 15:34:18 +0800

    feat: 补充部分接口

--- consts/consts.go
+++ consts/consts.go
@@ -170,6 +170,10 @@ var NGINX_INGRESS_APIS = map[string][]API{
 			Api:   "/api/feed/v1/search_history/delete",
 			Alias: "【订阅】【后端】删除搜索记录",
 		},
+		{
+			Api:   "/api/feed/v1/subscription/get",
+			Alias: "【订阅】【后端】获取订阅信息",
+		},
 	},
 	//HOST_LINGO_PRE_PUBLIC_BACKEND: {
 	//	{
@@ -217,6 +221,18 @@ var NGINX_INGRESS_APIS = map[string][]API{
 		{
 			Api:   "/api/plugin/articles/summary/list_v2",
 			Alias: "【单文档】【后端】刷新模型生成内容(list_v2)",
+		},
+		{
+			Api:   "/api/novel_form/get",
+			Alias: "【新形态内容】【后端】新形态内容获取",
+		},
+		{
+			Api:   "/api/novel_form/resource/get",
+			Alias: "【新形态内容】【后端】获取新形态绑定资源详情",
+		},
+		{
+			Api:   "/api/novel_form/feed/recommend",
+			Alias: "【新形态内容】【后端】读后推荐",
 		},
 	},
 	//HOST_LINGO_PRE_BACKEND: {

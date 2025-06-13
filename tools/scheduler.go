e2c002280e84a9a6e3e93dd697e5d9765554b93c
wenhao <hao.wen@deeplang.ai>
2025-06-13 23:19:01 +0800

    fix(调度器): 在预发环境避免两日交界时刷新数据
    添加预发环境检查，当处于23:55-23:59或00:00-00:04时间段时不执行数据刷新操作，防止跨日数据错误

--- tools/scheduler.go
+++ tools/scheduler.go
@@ -1,11 +1,13 @@
 package tools
 
 import (
+	constslib "codeup.aliyun.com/deeplang/lingowhale/lingowhale_backend/go_lib/consts"
 	"context"
 	"empyrean_lens/consts"
 	"empyrean_lens/service/aliyun"
 	"empyrean_lens/service/mongo/empyrean_lens"
 	"empyrean_lens/service/shence"
+	"os"
 	"time"
 
 	"github.com/cloudwego/hertz/pkg/common/hlog"
@@ -97,6 +99,14 @@ func (self *ProbeRunner) Run(ctx context.Context) {
 
 	// 每1分钟刷新一下当天的最新数据
 	s.Every(1).Minutes().StartImmediately().Do(func() {
+		if modeEnv := os.Getenv(constslib.ModeEnvName); modeEnv == constslib.ModeEnvPre {
+			// 如果是今天最后5分钟、今天前5分钟，则不刷新
+			if now := time.Now(); (now.Hour() == 23 && now.Minute() >= 55) ||
+				(now.Hour() == 0 && now.Minute() <= 4) {
+				hlog.CtxInfof(ctx, "pre环境，当前时间不在刷新时间范围内，跳过刷新，避免跨天刷错数据")
+				return
+			}
+		}
 		//empyrean_lens.UpdateLatestScoreInfo(ctx)
 		aliyun.CreateOrUpdateDatabase(ctx, consts.TIMESPAN_TODAY, false)
 		empyrean_lens.UpdateLatestScoreInfo(ctx) // 更新当天的分数同比、环比信息

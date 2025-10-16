2264f7df8867c1ee48f335473a1500737961393f
蒋思奇 <siqi.jiang@deeplang.ai>
2025-10-16 19:50:49 +0800

    fix: 完善新旧数据冲突-导致访问不安全的地址的处理

--- biz/handler/empyrean_lens/rentention.go
+++ biz/handler/empyrean_lens/rentention.go
@@ -469,12 +469,12 @@ func SystemDailyApiSlowInfo(ctx context.Context, c *app.RequestContext) {
 		costs[0] = strings.Split(r["Costs"], ",")
 		avg := make([]float64, 3)
 		// 增加空值处理
-		if len(result1Day) != 0 {
+		if i < len(result1Day) {
 			costs[1] = strings.Split(result1Day[i]["Costs"], ",")
 		} else {
 			costs[1] = []string{"0.0"}
 		}
-		if len(result7Day) != 0 {
+		if i < len(result7Day) {
 			costs[2] = strings.Split(result7Day[i]["Costs"], ",")
 		} else {
 			costs[2] = []string{"0.0"}
